89fc13d534a410106eb9dc9fbd3d654ca2c60c66
Evan Katz <evanakatz@gmail.com>
2019-07-05 18:56:56 -0400

    update

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -1,11 +1,12 @@
 import random
 
-games = [1 , 3, 4 ,5, 6, 7, 8, 9, 10]
+lives = 3
+count = 0
 
-for game in games:
+while lives > 0:
 
-    player1 = input('Player 1, make your move: ').lower()
-
+    print(f"You have {lives} lives, play wisely")
+    player1 = input('Make your move: ').lower()
     rand_num = random.randint(0,2)
 
     if rand_num == 0:
@@ -15,29 +16,52 @@ for game in games:
     elif rand_num == 2:
         player2 = 'paper'
 
-    print("The computer played:", player2)
+    print("The computer played: ", player2)
 
     if player1 == 'rock':
         if player2 == 'scissors':
-            print('Player 1 wins!')
+            print('You win!')
+            count += 1
+            print(count,' wins')
         if player2 == 'paper':
-            print('Player 2 wins!')
+            print('Computer wins!')
+            lives -= 1
+            if lives < 1:
+                play_again = input('Do you want to play again? (y/n) ')
+                if play_again == 'y':
+                    lives += 3
         if player2 == 'rock':
             print('Redo!')
     elif player1 == 'paper':
         if player2 == 'rock':
-            print('Player 1 wins')
+            print('You win!')
+            count += 1
+            print(count,' wins')
         if player2 == 'scissors':
-            print('Player 2 wins!')
+            print('Computer wins!')
+            lives -= 1
+            if lives < 1:
+                play_again = input('Do you want to play again? (y/n) ')
+                if play_again == 'y':
+                    lives += 3
         if player2 == 'paper':
             print('Redo!')
     elif player1 == 'scissors':
         if player2 == 'rock':
-            print('Player 2 wins!')
+            print('Computer wins!')
+            lives -= 1
+            if lives < 1:
+                play_again = input('Do you want to play again? (y/n) ')
+                if play_again == 'y':
+                    lives += 3
         if player2 == 'paper':
-            print('Player 1 wins!')
+            print('You win!')
+            count += 1
+            print(count,' wins')
         if player2 == 'scissors':
             print('Redo!')
+
     else:
         print('You spelled something wrong...')
+
 print('Hope you had some fun! Game over!')
